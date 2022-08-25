fe489dc3ea4005078fd284bd0bc1de098578828c
任亚楠 <renyanan@sfmail.sf-express.com>
2022-08-25 10:08:36 +0800

    更新文档首页

--- HOME/docs/.vuepress/config.ts
+++ HOME/docs/.vuepress/config.ts
@@ -1,7 +1,39 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme, defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: 'feer.icu',
+  description: 'feer.icu文档',
+  base: '/babel/',
+  theme: defaultTheme({
+    repo: 'https://github.com/feericu/',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdatedText: '上次更新',
+    contributors: false,
+    contributorsText: '贡献者',
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+    notFound: [
+      '这里什么都没有',
+      '我们怎么到这来了？',
+      '这是一个 404 页面',
+      '看起来我们进入了错误的链接',
+    ],
+    backToHome: '返回首页',
+    // a11y
+    openInNewWindow: '在新窗口打开',
+    toggleColorMode: '切换颜色模式',
+    toggleSidebar: '切换侧边栏',
+  }),
+  plugins: [
+    // backToTopPlugin(),
+    // docsearchPlugin({
+    //   // 配置
+    //   apiKey: '1',
+    //   indexName: 'babel中文',
+    //   appId: '2',
+    //   placeholder: '搜索文档'
+    // })
+  ]
 })
